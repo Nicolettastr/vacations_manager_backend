3b356f7625640e05ad2e565ed87756e5998ab964
Nicolettastr <nicolettastruggia@gmail.com>
2025-10-12 13:00:15 +0200

    changin port listener

--- index.js
+++ index.js
@@ -381,4 +381,6 @@ app.delete("/api/leaves/:id", authenticateToken, async (req, res) => {
 // START SERVER
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
