97fe78482f549a0524c8782c933eefcd78fe5a10
Nicolettastr <nicolettastruggia@gmail.com>
2025-11-28 18:19:13 +0100

    adding cors policy configuration

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "*", credentials: true }));
+
+const corsOptions = {
+  origin: ["http://localhost:9002"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.options("*", cors(corsOptions));
 
 const authRoutes = require("./routes/auth");
 const employeeRoutes = require("./routes/employees");
